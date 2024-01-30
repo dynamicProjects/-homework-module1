93b111eb3023ece824a94020373bc6d1d5d25a17
shruti-source <shrutishrivastav77@gmail.com>
2024-01-29 22:59:19 -0600

    created new js filr randomcodes.js and added generate code function to it

---
+++ js/randomcodes.js
@@ -0,0 +1,11 @@
+function generateCode(){
+    var code = '';
+    var str = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789@#$';
+    // genrate multiple characters using loop
+    for(var i=0; i<=8; i++){
+        var char = Math.random()*str.length;
+        code+=str.charAt(char)
+    }
+    return code;
+}
+document.getElementById("codes").innerHTML=generateCode();
